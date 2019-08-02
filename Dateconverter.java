b891b5874413ae4accfb4dcf904aaad1b38107b8
nandajaga <nandaece69@gmail.com>
2019-08-02 10:48:05 +0530

    Update Dateconverter.java

--- Dateconverter.java
+++ Dateconverter.java
@@ -7,20 +7,28 @@ import java.util.Date;
 
 import org.junit.Test;
 
-public class Dateconverted {
+//Use below method to convert date to any format by modifying highlighted one.
 
-// converts 08/01/19 to 2019-08-01
-	
-	public static void Dateconverter(String Date) throws ParseException
-    {
-           Date DF = new Date();
-           DateFormat FromDF = new SimpleDateFormat("MM/dd/yy");
-     FromDF.setLenient(false);  // this is important!
-     Date FromDate = FromDF.parse(Date);   //"08/01/19"
-     String dateStringFrom = new SimpleDateFormat("yyyy-MM-dd").format(FromDate);
-     System.out.println(dateStringFrom);
-     //return dateStringFrom;
-    }
+//08/01/19 to 2019-08-01
+
+
+//Dateconverter("08/01/19");
+
+public String Dateconverter(String Date) throws ParseException
+       {
+              Date DF = new Date();
+              DateFormat FromDF = new SimpleDateFormat("MM/dd/yy"); //format change
+        FromDF.setLenient(false);  // this is important!
+        Date FromDate = FromDF.parse(Date);   //"08/01/19"
+        String dateStringFrom = new SimpleDateFormat("yyyy-MM-dd").format(FromDate); //format change
+        System.out.println(dateStringFrom);
+        return dateStringFrom;
+       
+O/p:
+
+2019-08-01
+
+
 	
 	@Test
 	public void test() throws ParseException {
